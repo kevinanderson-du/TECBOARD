a7b6aa0b033ddf83bd71eb11892b5c3f7a08fde3
unknown <kevinbarcelona27@gmail.com>
2026-03-03 20:28:15 -0300

    Estilizando caixa de formulario

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -3,16 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './Juninho_Pernambucano.css'
 
+function Box_de_formulario () {
+  return (
+    <form className='box'>
+      <h2>Preencha seus dados:</h2>
+      <fieldset>
+        <label>
+          Nome:
+        </label>
+        <input type='text' id='nome'/>
+      </fieldset>
+    </form>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main>
+      <header>
       <div>
         <h1>Hello world</h1>
       </div>
-      
-    </>
+      </header>
+      <Box_de_formulario/>
+    </main>
   )
 }
 
