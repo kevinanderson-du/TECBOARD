9228fe580a5ffa128179d3a09d470ea12e649653
unknown <kevinbarcelona27@gmail.com>
2026-03-08 11:37:48 -0300

    Adicionando novo box com botão

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -70,7 +70,13 @@ function App() {
       <section className='section'>
         <img className='image' src="/Mulher_Com_Oculos.png" alt=''/>
       </section>
+      
       <Box_de_formulario/>
+      <div className='div'>
+        <h1 className='acesso'>Acesso</h1>
+        <h3 className='acesso'>Tenha acesso a vários benefícios exclusivos!</h3>
+        <input className='botao' type='button' id='acesso' value={"Acessar"}/>
+      </div>
       <Rodape/>
     </main>
   )
