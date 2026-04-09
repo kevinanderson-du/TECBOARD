a9e3e2ef753c7344110a2930bed89609eb12643a
unknown <kevinbarcelona27@gmail.com>
2026-04-09 12:00:09 -0300

    Estilizando Card

--- tecboard/src/componentes/Card.jsx
+++ tecboard/src/componentes/Card.jsx
@@ -5,12 +5,18 @@ export function Card({evento}) {
         <div className={styles.card_container}>
             <img className={styles.imagem} src={evento.capa} alt={evento.titulo}/>
             <div className={styles.tag}>
+
                 <p className={styles.tema}>{evento.mini_card.nome} </p>
-                <p>
+                
+
+                <h4 className={styles.mulheres_no_front}>{evento.titulo}</h4>
+
+                <h5 className={styles.desc}>{evento.desc}</h5>
+
+                <p className={styles.data}>
+                    <img className={styles.icone_calendario} src='/ICONE CALENDARIO.png' alt=''/>
                     {evento.data.toLocaleDateString('pt-BR')}
                 </p>
-                <h4 className={styles.mulheres_no_front}>{evento.titulo}</h4>
-                <h5>{evento.desc}</h5>
             </div>
         </div>
     )
